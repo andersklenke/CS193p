3fdbd19389699ab889be5dae47c5e590926304a6
Anders Klenke <anders_klenke@icloud.com>
2015-01-28 14:39:14 +0100

    Lecture 2
    More Xcode and Swift, MVC

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -11,7 +11,17 @@ import UIKit
 class ViewController: UIViewController {
   @IBOutlet weak var display: UILabel!
   
-  var userIsInTheMiddleOfTypingAnumber: Bool = false
+  var userIsInTheMiddleOfTypingAnumber = false
+  var operandStack = Array<Double>()
+  var displayValue: Double {
+    get {
+      return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+    }
+    set {
+      display.text = "\(newValue)"
+      userIsInTheMiddleOfTypingAnumber = false
+    }
+  }
   
   @IBAction func appendDigit(sender: UIButton) {
     let digit = sender.currentTitle!
@@ -21,6 +31,43 @@ class ViewController: UIViewController {
     } else {
       display.text = digit
       userIsInTheMiddleOfTypingAnumber = true
+    }
+  }
+  
+  @IBAction func enter() {
+    userIsInTheMiddleOfTypingAnumber = false
+    operandStack.append(displayValue)
+    println("operandStack = \(operandStack)")
+  }
+  
+  @IBAction func operate(sender: UIButton) {
+    let operation = sender.currentTitle!
+    
+    if userIsInTheMiddleOfTypingAnumber {
+      enter()
+    }
+    
+    switch operation {
+    case "×": performOperation { $0 * $1 }
+    case "÷": performOperation { $1 / $0 }
+    case "+": performOperation { $0 + $1 }
+    case "−": performOperation { $1 - $0 }
+    case "√": performOperation { sqrt($0) }
+    default: break
+    }
+  }
+  
+  func performOperation(operation: Double -> Double) {
+    if operandStack.count >= 2 {
+      displayValue = operation(operandStack.removeLast())
+      enter()
+    }
+  }
+  
+  func performOperation(operation: (Double, Double) -> Double) {
+    if operandStack.count >= 2 {
+      displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+      enter()
     }
   }
 }
